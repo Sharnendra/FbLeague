c246c9e9ce6b23166099836c62ed63631e9e4189
Sharnendra <42963698+Sharnendra@users.noreply.github.com>
2021-04-22 21:14:22 +0530

    Add files via upload
    Primary Push

---
+++ src/main/java/com/footballleague/sapient/FbLeague.java
@@ -0,0 +1,13 @@
+package com.footballleague.sapient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbLeague {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbLeague.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballleague/sapient/FbLeagueTests.java
@@ -0,0 +1,13 @@
+package com.footballleague.sapient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbLeagueTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
