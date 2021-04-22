c969604aae83b843a7a92cc20566a1496f0367c4
Sharnendra <sharnendradey@gmail.com>
2021-04-23 03:05:46 +0530

    Static resource setup to display the web pages from certain path as given in yml.

---
+++ src/main/java/com/footballleague/sapient/configuration/StaticResourceConfiguration.java
@@ -0,0 +1,22 @@
+package com.footballleague.sapient.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Value("${resources.windows-resource}")
+	private String windowResource;
+	
+	@Value("${resources.linux-resource}")
+	private String linuxResource;
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/footballStranding/**").addResourceLocations(windowResource).addResourceLocations(linuxResource);
+    }
+}
